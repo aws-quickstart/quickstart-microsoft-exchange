c9d06b062a72f0af883ff2a7b9715f52892f0567
Santiago Cardenas <sancard@amazon.com>
2017-01-30 18:18:57 -0800

    Update to CU15. Generalize executable name.

--- scripts/Expand-ExchangeFiles.ps1
+++ scripts/Expand-ExchangeFiles.ps1
@@ -1,4 +1,4 @@
-﻿[CmdletBinding()]
+[CmdletBinding()]
 param(
     [string]
     $ComputerName,
@@ -14,7 +14,7 @@ try {
     $pass = ConvertTo-SecureString $Password -AsPlainText -Force
     $cred = New-Object System.Management.Automation.PSCredential -ArgumentList $UserName,$pass
         
-    Invoke-Command -ScriptBlock {Start-Process cmd.exe '/c c:\exchinstall\Exchange2013-x64-cu7.exe /extract:c:\exchinstall /passive' -NoNewWindow -Wait} -ComputerName $ComputerName -Credential $cred
+    Invoke-Command -ScriptBlock {Start-Process cmd.exe '/c c:\exchinstall\Exchange2013-x64.exe /extract:c:\exchinstall /passive' -NoNewWindow -Wait} -ComputerName $ComputerName -Credential $cred
 }
 catch {
     $_ | Write-AWSQuickStartException
