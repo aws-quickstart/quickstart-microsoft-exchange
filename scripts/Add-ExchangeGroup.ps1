14908e79fc4b4151ba58eaa524d486eba26347bb
Dragos Madarasan <madarasa@amazon.com>
2017-06-09 19:55:32 +0300

    Finished DAG implementation. Removed Set-DNS/DNSsuffix due to DHCP options in use

---
+++ scripts/Add-ExchangeGroup.ps1
@@ -0,0 +1,34 @@
+[CmdletBinding()]
+param(
+
+    [Parameter(Mandatory=$true)]
+    [string]
+    $DomainNetBIOSName,
+
+    [Parameter(Mandatory=$true)]
+    [string]
+    $ExchangeGroupName,
+
+	[Parameter(Mandatory=$True)]
+	[string]
+	$ServerName,
+
+    [Parameter(Mandatory=$True)]
+	[string]
+	$LocalGroupName
+)
+
+
+try {
+	Start-Transcript -Path C:\cfn\log\Add-ExchangeGroup.ps1.txt -Append
+    $ErrorActionPreference = "Stop"
+    
+    $ExchangeGroup = [ADSI]"WinNT://$DomainNetBIOSName/$ExchangeGroupName,group" 
+    $LocalGroup = [ADSI]"WinNT://$ServerName/$LocalGroupName,group" 
+    $LocalGroup.PSBase.Invoke("Add",$ExchangeGroup.PSBase.Path) 
+        
+}
+catch {
+    Write-Verbose "$($_.exception.message)@ $(Get-Date)"
+    $_ | Write-AWSQuickStartException
+}
---
+++ scripts/Install-NetFramework.ps1
@@ -0,0 +1,30 @@
+﻿[CmdletBinding()]
+param()
+
+try {
+    Start-Transcript -Path C:\cfn\log\Install-NetFramework.ps1.txt -Append
+    $ErrorActionPreference = "Stop"
+	
+	$Retries = 0
+    $Installed = $false
+	while (($Retries -lt 4) -and (!$Installed)) {
+        try {
+            Invoke-Expression "C:\Exchangeinstall\NDP462-KB3151800-x86-x64-AllOS-ENU.exe /q /norestart" -ErrorAction Stop
+            $Installed = $true
+        }
+        catch {
+            $Exception = $_
+            $Retries++
+            if ($Retries -lt 4) {
+                Start-Sleep ($Retries * 60)
+            }
+        }
+    }
+    if (!$Installed) {
+          throw $Exception
+    }
+	
+}
+catch {
+    $_ | Write-AWSQuickStartException
+}
