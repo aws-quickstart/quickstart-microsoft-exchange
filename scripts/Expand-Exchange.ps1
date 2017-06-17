6d6ec57e131e44560e38e565a7d4fab6ca7c3044
Dragos Madarasan <madarasa@amazon.com>
2017-06-18 00:41:03 +0100

    Moved to .Net Framework 4.6.2 and latest Exchange Cumulative updates WSFCNodes: Cleaned up security group ingress rules, Removed Invoke-ADReplication Edge role: re-added set-DnsSuffix, moved create-edge-dnsrecord to install, added correct security group rules

--- scripts/Expand-Exchange.ps1
+++ scripts/Expand-Exchange.ps1
@@ -12,11 +12,15 @@ try {
     $ErrorActionPreference = "Stop"
 	
 	if($ExchangeServerVersion -eq "2013") {
-        Invoke-Command -ScriptBlock {Start-Process cmd.exe '/c c:\Exchangeinstall\Exchange2013-x64-cu15.exe /extract:c:\Exchangeinstall /passive' -NoNewWindow -Wait} 
-        #Invoke-Command -ScriptBlock {Start-Process cmd.exe '/c c:\Exchangeinstall\Exchange2013-x64-cu16.exe /extract:c:\Exchangeinstall /passive' -NoNewWindow -Wait} 
+        #Invoke-Command -ScriptBlock {Start-Process cmd.exe '/c c:\Exchangeinstall\Exchange2013-x64-cu15.exe /extract:c:\Exchangeinstall /passive' -NoNewWindow -Wait} 
+        Invoke-Command -ScriptBlock {Start-Process cmd.exe '/c c:\Exchangeinstall\Exchange2013-x64-cu16.exe /extract:c:\Exchangeinstall /passive' -NoNewWindow -Wait} 
     }
     elseif ($ExchangeServerVersion -eq "2016") {
-        Invoke-Command -ScriptBlock {Start-Process cmd.exe '/c c:\Exchangeinstall\Exchange2016-x64.exe /extract:c:\Exchangeinstall /passive' -NoNewWindow -Wait} 		
+        #Invoke-Command -ScriptBlock {Start-Process cmd.exe '/c c:\Exchangeinstall\Exchange2016-x64.exe /extract:c:\Exchangeinstall /passive' -NoNewWindow -Wait} 
+		$isoPath = 'C:\Exchangeinstall\ExchangeServer2016-x64-cu5.iso'
+		Mount-DiskImage -ImagePath $isoPath
+		$driveLetter = (Get-DiskImage $isoPath | Get-Volume).DriveLetter
+		Copy-Item -Path "${driveLetter}:\*" -Destination 'C:\Exchangeinstall' -Recurse
     }
 	
     
