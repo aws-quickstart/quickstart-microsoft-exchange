c3310e4f8850eea7a20c65072acce13a17a9f8dc
Santiago Cardenas <sancard@amazon.com>
2017-08-09 20:22:09 -0700

    Readd Create-Share since it's used in old version

---
+++ scripts/Create-Share.ps1
@@ -0,0 +1,29 @@
+﻿[CmdletBinding()]
+param(
+    [string]
+    $ComputerName,
+
+    [string]
+    $UserName,
+
+    [string]
+    $Password,
+
+    [string]
+    $FolderName,
+
+    [string]
+    $ShareName
+)
+
+try {
+    $pass = ConvertTo-SecureString $Password -AsPlainText -Force
+    $cred = New-Object System.Management.Automation.PSCredential -ArgumentList $UserName,$pass
+    
+    $sb = { param($path,$share) New-SmbShare -Name $share -Path $path -FullAccess everyone }
+        
+    Invoke-Command -ScriptBlock $sb -ArgumentList $FolderName,$ShareName -ComputerName $ComputerName -Credential $cred -ErrorAction Stop
+}
+catch {
+    $_ | Write-AWSQuickStartException
+}
