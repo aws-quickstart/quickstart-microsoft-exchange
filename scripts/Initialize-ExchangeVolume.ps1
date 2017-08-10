01bfb2afdadec5dfb6132916b4818dd5ce186238
Santiago Cardenas <sancard@amazon.com>
2017-08-09 18:08:09 -0700

    Find drive letter for volume formatting dynamically

--- scripts/Initialize-ExchangeVolume.ps1
+++ scripts/Initialize-ExchangeVolume.ps1
@@ -5,8 +5,17 @@ param(
     $DriveLetter
 )
 try {
-    Format-Volume -DriveLetter d -FileSystem ntfs -NewFileSystemLabel 'Exchange Databases' -AllocationUnitSize 65536 -Confirm:$false -Force -ErrorAction stop
+    $ErrorActionPreference = "Stop"
+
+    $driveLetter = Get-Volume | ? { $_.DriveLetter -notin @('','C') -and $_.FileSystemLabel -notlike "Temporary*" } | select -ExpandProperty DriveLetter
+    if ($driveLetter.Count -lt 1) {
+        throw "Volume not found."
+    }
+    elseif ($driveLetter.Count -gt 1) {
+        throw "More than one volume found: $driveLetter"
+    }
+    Format-Volume -DriveLetter $driveLetter -FileSystem ntfs -NewFileSystemLabel 'Exchange Databases' -AllocationUnitSize 65536 -Confirm:$false -Force -ErrorAction stop
 }
 catch {
-    $_ | Write-AWSQuickStartException    
+    $_ | Write-AWSQuickStartException
 }
